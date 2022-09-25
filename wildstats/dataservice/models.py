aa8db358d839c52803116232e93f1fcf8486b91e
leoflewis <fleonardolewis@gmail.com>
2022-09-24 20:21:32 -0500

    added models

--- wildstats/dataservice/models.py
+++ wildstats/dataservice/models.py
@@ -6,3 +6,12 @@ import requests
 
 def get_roster():
     response = requests.get("https://statsapi.web.nhl.com/api/v1/teams/30/roster")
+    return response
+
+def get_schedule():
+    response = requests.get("https://statsapi.web.nhl.com/api/v1/schedule?teamId=30&season=20222023")
+    return response
+
+def get_player(id):
+    response = requests.get("https://statsapi.web.nhl.com/api/v1/people/{}/stats?stats=gameLog".format(id))
+    return response
