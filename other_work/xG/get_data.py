e2fcb2c70a13557b57818d096dbccb887d58f197
leoflewis <fleonardolewis@gmail.com>
2022-11-16 18:59:46 -0600

    added dummies for shot type

--- other_work/xG/get_data.py
+++ other_work/xG/get_data.py
@@ -12,8 +12,7 @@ from joblib import dump
 
 # Scrape data
 print("Begining to scrape pbp. No shifts. Depending on what timerange was requested, this could take awhile.")
-data = hockey_scraper.scrape_games([2022020017, 2022020034, 2022020045, 2022020067, 2022020074, 2022020101, 2022020113, 2022020132,
-2022020140, 2022020151, 2022020167, 2022020206, 2022020210, 2022020224, 2022020240], False, data_format = 'Pandas')
+data = hockey_scraper.scrape_games([2022020017, 2022020034], False, data_format = 'Pandas')
 print("Finished scraping.")
 
 # Access dataframe
@@ -32,10 +31,12 @@ df.drop(['Game_Id', 'Date', 'Description', 'Time_Elapsed',
        'homePlayer6', 'homePlayer6_id', 'Away_Players', 'Home_Players',
        'Away_Score', 'Home_Score', 'Away_Goalie_Id',
        'Home_Goalie_Id', 'Home_Coach',
-       'Away_Coach'], axis=1, inplace=True)
+       'Away_Coach', 'Home_Zone', 'p1_name'], axis=1, inplace=True)
+
 
 # Drop empty values, this will get rid of lots of unecessary events. It will also get rid of empty net goals.
 df.dropna(inplace=True)
+
 
 # Drop worthless events, also drop shootout events
 events = df[ (df.Event == 'FAC') | (df.Event == 'BLOCK') | 
@@ -44,16 +45,19 @@ events = df[ (df.Event == 'FAC') | (df.Event == 'BLOCK') |
 
 df.drop(events, inplace=True)
 
+# Add a binary column for Goals 
+df['Goal'] = numpy.where(df.Event == 'GOAL', 1, 0)
+
+df.drop(['Away_Goalie', 'Home_Goalie', 'Event', 'Strength'], axis=1, inplace=True)
+
 # We need to transpose goals/shots/misses so it reads as if they all happen on the same net.
 # So we will tranpose the shot location if it is in the negative half of the rink.
 # Technically this could incorrecly move some shots that occur form over the red line.
-# Those dont happen very often and we alreay removed empty net goals, so there shouldnt be any goals from beyond the red line.
+# Those dont happen very often and we already removed empty net goals, so there shouldnt be any goals from beyond the red line.
+# This will add a couple shots that take place behind the net, but they will never be goals, so it wont give them a good score.
 # Overall this could add a little extra noise, but it should not be anything too serious. 
 df.loc[(df['xC'] < 0), 'yC'] = df['yC'] * -1
 df.loc[(df['xC'] < 0), 'xC'] = df['xC'] * -1
-
-# Add a binary column for Goals 
-df['Goal'] = numpy.where(df.Event == 'GOAL', 1, 0)
 
 # This function calculates the angle to the center of the net at (89, 0) in radians and degrees.
 def angles(x, y):
@@ -62,6 +66,11 @@ def angles(x, y):
     degrees = (radians * 180.0) / 3.14
     arr = [radians, degrees]
     return arr
+
+
+# We will employ one categorical variable, the shot type.
+# The get_dummies function will create columns for a binary labeling of all 8 of the shot types. 
+df = pandas.get_dummies(df)
 
 # Initiliaze empty columns
 df['Angle_Radians'] = ''
@@ -88,30 +97,35 @@ print(df['Distance'].mean())
 
 # Data should be good to go at this point. 
 # We will build the model
-predictors = ['xC', 'yC', 'Angle_Radians', 'Angle_Degrees', 'Distance']
-x = df[predictors] 
+predictors = ['xC', 'yC', 'Type_', 'Type_BACKHAND', 'Type_DEFLECTED', 'Type_SLAP SHOT', 'Type_SNAP SHOT', 'Type_TIP-IN', 'Type_WRAP-AROUND', 'Type_WRIST SHOT', 'Angle_Radians', 'Angle_Degrees', 'Distance']
+x = df[predictors]
 y = df.Goal
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size =.2, random_state=16)
 model = LogisticRegression()
 model.fit(x_train, y_train)
 score = model.score(x_test, y_test)
 predictions = model.predict(x_test)
-print('goals ' + str(len(df.Event=='GOAL')))
-print('fenwick shots' + str(len(df.Event)))
 print(score)
 cm = metrics.confusion_matrix(y_test, predictions)
+print("confusion matrix: ")
 print(cm)
-print(model.coef_)
+print("coefficients: "+ str(model.coef_))
+
+# Our columns after modeling are as follows:
+# Period, xC, yC, Goal, Type_, Type_BACKHAND, Type_DEFLECTED, Type_SLAP SHOT, Type_SNAP SHOT, Type_TIP-IN, Type_WRAP-AROUND, Type_WRIST SHOT, Angle_Radians, Angle_Degrees, Distance
 
 x = 86
 y = -6
 new_angles = angles(x, y)
 new_distance = numpy.sqrt((y - 0)**2 + (x - 89.0)**2)
-new_shot = [[x, y, new_angles[0], new_angles[1], new_distance]]
+new_shot = [[x, y, 0, 0, 0, 1, 0, 0, 0, 0, new_angles[0], new_angles[1], new_distance]]
 new_df = pandas.DataFrame(new_shot, columns=predictors)
 pred = model.predict_proba(new_df) 
-print(pred)
-df.loc[len(df.index)] = [0, "NET", "", "NET", "OFF", "", "", "", 89, 0, -1, 0, 0, 0]
+print("odds of 0 and 1: " + str(pred))
+
+
+# Adding another point with -1 goal value that serve as the net in the graph created below
+df.loc[len(df.index)] = [0, 89, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 sns.scatterplot(x='xC', y='yC', hue='Goal', marker='o', data=df)
 plt.savefig("xG.png")
 dump(model, 'xG.joblib')
