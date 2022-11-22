8471f4d20be726b8b2151b073edea5449f61c943
leoflewis <fleonardolewis@gmail.com>
2022-11-21 18:39:41 -0600

    added power play and rebound columns

--- other_work/xG/get_data.py
+++ other_work/xG/get_data.py
@@ -14,15 +14,17 @@ from joblib import dump
 
 # Scrape data
 print("Begining to scrape pbp. No shifts. Depending on what timerange was requested, this could take awhile.")
-data = hockey_scraper.scrape_seasons([2021], False, data_format = 'Pandas')
+data = hockey_scraper.scrape_games([2022020283], False, data_format = 'Pandas')
 print("Finished scraping.")
 
 # Access dataframe
 df = data['pbp']
 
+home = df.Home_Team[0]
+away = df.Away_Team[0]
+
 # Drop unnecessary columns
-df.drop(['Date', 'Description', 'Time_Elapsed', 'Ev_Zone', 'Ev_Team',
-        'Away_Team', 'Home_Team', 'p1_ID', 'p2_name',
+df.drop(['Date', 'Description', 'Time_Elapsed', 'Ev_Zone', 'p1_ID', 'p2_name',
        'p2_ID', 'p3_name', 'p3_ID', 'awayPlayer1', 'awayPlayer1_id',
        'awayPlayer2', 'awayPlayer2_id', 'awayPlayer3', 'awayPlayer3_id',
        'awayPlayer4', 'awayPlayer4_id', 'awayPlayer5', 'awayPlayer5_id',
@@ -34,34 +36,25 @@ df.drop(['Date', 'Description', 'Time_Elapsed', 'Ev_Zone', 'Ev_Team',
        'Home_Goalie_Id', 'Home_Coach',
        'Away_Coach', 'Home_Zone', 'p1_name'], axis=1, inplace=True)
 
-
 # Make a new column for whether or not a shot is a rebound.
 # If same game and same period as the previous row, and margin of time elapsed between events is <= 4 mark 1 for rebound
-
-
-
-
-# Make a new column for whether or not its a power play goal. 
-# If event team is home and int(df.Strength[0]) > int(df.Strength[2]), mark man advantage, else mark 0
-# If event team is away and int(df.Strength[0]) < int(df.Strength[2]), mark man advantage, else mark 0
-
+df['Rebound'] = numpy.where((df.Game_Id == df.Game_Id.shift(1)) & (df.Period == df.Period.shift(1)) & (df.Ev_Team == df.Ev_Team.shift(1)) & (df.Seconds_Elapsed - df.Seconds_Elapsed.shift(1) <= 3), 1, 0)
 
 # Drop empty values, this will get rid of lots of unecessary events. It will also get rid of empty net goals.
 df.dropna(inplace=True)
 
+# Make a new column for whether or not its a power play goal. 
+df['Power Play'] = numpy.where( ((df.Ev_Team == home) & (df.Strength.str[0] > df.Strength.str[2])) | ((df.Ev_Team == away) & (df.Strength.str[0] < df.Strength.str[2])), 1, 0)
 
 # Drop worthless events, also drop shootout events
-events = df[ (df.Event == 'FAC') | (df.Event == 'BLOCK') | 
-    (df.Event == 'PENL') | (df.Event == 'GIVE') | (df.Event == 'TAKE') | 
-    (df.Event == 'STOP') | (df.Event == 'HIT') | (df.Period == 5)].index
-
+events = df[ (df.Event == 'FAC') | (df.Event == 'BLOCK') | (df.Event == 'PENL') | (df.Event == 'GIVE') | (df.Event == 'TAKE') | (df.Event == 'STOP') | (df.Event == 'HIT') | (df.Period == 5)].index
 df.drop(events, inplace=True)
 
 # Add a binary column for Goals 
 df['Goal'] = numpy.where(df.Event == 'GOAL', 1, 0)
 
 # We no longer need these columns and can drop them
-df.drop(['Away_Goalie', 'Home_Goalie', 'Event', 'Strength'], axis=1, inplace=True)
+df.drop(['Away_Goalie', 'Home_Goalie', 'Strength', 'Game_Id', 'Seconds_Elapsed', 'Away_Team', 'Home_Team', 'Event', 'Ev_Team', 'Period'], axis=1, inplace=True)
 
 # We need to transpose goals/shots/misses so it reads as if they all happen on the same net.
 # So we will tranpose the shot location if it is in the negative half of the rink.
@@ -69,7 +62,7 @@ df.drop(['Away_Goalie', 'Home_Goalie', 'Event', 'Strength'], axis=1, inplace=Tru
 # Those dont happen very often and we already removed empty net goals, so there shouldnt be any goals from beyond the red line.
 # This will add a couple shots that take place behind the net, but they will never be goals, so it wont give them a good score.
 # Overall this could add a little extra noise, but it should not be anything too serious. 
-df.loc[((df['xC'] < 0) and (df['yC'] < 0)), 'yC'] = df['yC'] * -1
+df.loc[((df['xC'] < 0) & (df['yC'] < 0)), 'yC'] = df['yC'] * -1
 df.loc[(df['xC'] < 0), 'xC'] = df['xC'] * -1
 
 # This function calculates the angle to the center of the net at (89, 0) in radians and degrees.
@@ -106,11 +99,9 @@ with pandas.option_context('display.max_rows', 1000,
                        ):
     print(df)
 
-print(df['Distance'].mean())
-
 # Data should be good to go at this point. 
 # We will build the model
-predictors = ['xC', 'yC', 'Type_', 'Type_BACKHAND', 'Type_DEFLECTED', 'Type_SLAP SHOT', 'Type_SNAP SHOT', 'Type_TIP-IN', 'Type_WRAP-AROUND', 'Type_WRIST SHOT', 'Angle_Radians', 'Angle_Degrees', 'Distance']
+predictors = ['xC', 'yC', 'Rebound', 'Power Play', 'Type_BACKHAND', 'Type_DEFLECTED', 'Type_SLAP SHOT', 'Type_SNAP SHOT', 'Type_TIP-IN', 'Type_WRAP-AROUND', 'Type_WRIST SHOT', 'Angle_Radians', 'Angle_Degrees', 'Distance', ]
 x = df[predictors]
 y = df.Goal
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size =.2, random_state=16)
@@ -125,15 +116,15 @@ print(cm)
 print("coefficients: "+ str(model.coef_))
 
 # Our columns after modeling are as follows:
-# Period, xC, yC, Goal, Type_, Type_BACKHAND, Type_DEFLECTED, Type_SLAP SHOT, Type_SNAP SHOT, Type_TIP-IN, Type_WRAP-AROUND, Type_WRIST SHOT, Angle_Radians, Angle_Degrees, Distance
+# xC, yC, Rebound, Power Play, Goal, Type_, Type_BACKHAND, Type_DEFLECTED, Type_SLAP SHOT, Type_SNAP SHOT, Type_TIP-IN, Type_WRAP-AROUND, Type_WRIST SHOT, Angle_Radians, Angle_Degrees, Distance
 
-
+print("***PREDICTION***")
 # Lets make a prediction
 x = 86
 y = -6
 new_angles = angles(x, y)
 new_distance = numpy.sqrt((y - 0)**2 + (x - 89.0)**2)
-new_shot = [[x, y, 0, 0, 0, 1, 0, 0, 0, 0, new_angles[0], new_angles[1], new_distance]]
+new_shot = [[x, y, 0, 0, 0, 0, 1, 0, 0, 0, 0, new_angles[0], new_angles[1], new_distance]]
 new_df = pandas.DataFrame(new_shot, columns=predictors)
 pred = model.predict_proba(new_df) 
 print("odds of 0 and 1: " + str(pred))
